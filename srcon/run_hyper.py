e227612fe40d90c25de5c7663d33a89ea7eae3bf
ztjiaweixu <ztjiaweixu@tencent.com>
2023-03-09 17:17:37 +0800

    udpate config

--- srcon/run_hyper.py
+++ srcon/run_hyper.py
@@ -19,26 +19,28 @@ def get_args():
         "--dataset",
         type=str,
         default="Branin",
-        help="[korea, chengdu, hanjiang, nandong] for scron, [Branin, Schwefel, Ackley] for blackbox",
+        help="[korea, chengdu, hanjiang, nandong] for scron, [Branin, Schwefel, Ackley, Hartmann] for blackbox",
     )
     parser.add_argument("--seed", type=int, default=2023)
     parser.add_argument("--n-exp", type=int, default=3)
     parser.add_argument("--debug", type=int, default=0, choices=[0, 1])
-    parser.add_argument("--time-period", type=int, default=int(1e6))
-    parser.add_argument("--d-theta", type=int, default=20)
+    parser.add_argument("--time-period", type=int, default=int(1e3))
     parser.add_argument("--d-index", type=int, default=16)
     parser.add_argument("--lr", type=float, default=0.001)
     parser.add_argument("--batch-size", type=int, default=128)
     parser.add_argument("--xi-coef", type=float, default=0.01)
     parser.add_argument("--norm-coef", type=float, default=0.01)
-    parser.add_argument("--hidden-sizes", type=int, nargs="+", default=[128])
+    parser.add_argument("--hidden-sizes", type=int, nargs="+", default=[256])
+    parser.add_argument("--prior-scale", type=float, default=10)
+    parser.add_argument("--posterior-scale", type=float, default=1)
     parser.add_argument("--update-freq", type=int, default=1)
-    parser.add_argument("--update-num", type=int, default=10)
-    parser.add_argument("--learning-start", type=int, default=2000)
+    parser.add_argument("--update-num", type=int, default=1)
+    parser.add_argument("--learning-start", type=int, default=100)
     parser.add_argument("--action-num", type=int, default=10)
+    parser.add_argument("--action-single-noise", type=int, default=1, choices=[0, 1])
     parser.add_argument("--action-lr", type=float, default=0.001)
-    parser.add_argument("--action-max-update", type=int, default=1000)
-    parser.add_argument("--log-freq", type=int, default=100)
+    parser.add_argument("--action-max-update", type=int, default=100)
+    parser.add_argument("--log-freq", type=int, default=10)
     parser.add_argument("--log-dir", type=str, default="./results/srcon")
     args = parser.parse_known_args()[0]
     return args
@@ -72,13 +74,13 @@ def plotLoss(title, path, log=False):
     plt.savefig(path + "/loss.pdf")
 
 
-def plotReward(title, path, log=False):
+def plotResult(title, path, log=False):
     x_data, y_data = [], []
     for file in os.listdir(path):
         if "progress" in file:
             data = pd.read_csv(os.path.join(path, file))
-            x_data.append(data["step"].to_numpy())
-            y_data.append(data["reward"].to_numpy())
+            x_data.append(data["step"].values)
+            y_data.append(data["best_reward"].values)
     x_data = np.array(x_data)
     y_data = np.array(y_data)
     x = x_data[0]
@@ -94,7 +96,7 @@ def plotReward(title, path, log=False):
         plt.yscale("log")
     plt.grid(color="grey", linestyle="--", linewidth=0.5)
     plt.title(title)
-    plt.ylabel("Step Reward")
+    plt.ylabel("Best Reward")
     plt.xlabel("Time period")
     plt.legend(loc="best")
     plt.savefig(path + "/reward.pdf")
@@ -123,14 +125,16 @@ def run(args, model, objective, logger, run_id):
     update_num = 0
     update_results = {}
     reward_list, error_list = [], []
-    best_reward, best_action = -np.inf, None
     maximize = True if args.env == "srcon" else False
+    best_reward = -np.inf if maximize else np.inf
     for t in range(1, args.time_period + 1):
         start_time = time.time()
         # collect data
         if t >= args.learning_start:
             xs = objective.sample_action(args.action_num)
-            x = model.select_action(xs, objective.bound_point, maximize)
+            x = model.select_action(
+                xs, objective.bound_point, args.action_single_noise, maximize
+            )
         else:
             x = objective.sample_action(1)
         x = objective.bound_point(x)
@@ -139,7 +143,9 @@ def run(args, model, objective, logger, run_id):
         y_pred = y_pred[0][0]
         reward_list.append(y_true)
         error_list.append(abs(y_true - y_pred))
-        if y_true > best_reward:
+        if (maximize and y_true > best_reward) or (
+            not maximize and y_true < best_reward
+        ):
             best_reward = y_true
             best_action = x
             logger.info(f"Step {t}, Best Reward {best_reward}.")
@@ -155,10 +161,12 @@ def run(args, model, objective, logger, run_id):
         end_time = time.time()
         # log data
         if t % args.log_freq == 0 or t == 1:
+            logger.record("run_id", run_id)
             logger.record("step", t)
             logger.record("time", end_time - start_time)
             logger.record("error", error_list[-1])
             logger.record("reward", reward_list[-1])
+            logger.record("best_reward", best_reward)
             logger.record("update_num", update_num)
             for key, val in update_results.items():
                 logger.record(key, val)
@@ -187,10 +195,9 @@ def main(args):
             objective = SRCON(args.dataset)
             args.d_theta = objective.input_dim
         elif args.env == "blackbox":
-            from blackbox import BlackBox
+            from blackbox import BlackBox, D_THETA
 
-            if args.dataset == "Branin":
-                args.d_theta = 2
+            args.d_theta = D_THETA[args.dataset]
             objective = BlackBox(args.dataset, args.d_theta)
 
         # create hypermodel
@@ -198,8 +205,10 @@ def main(args):
             args.d_index,
             args.d_theta,
             hidden_sizes=args.hidden_sizes,
-            lr=args.lr,
+            prior_scale=args.prior_scale,
+            posterior_scale=args.posterior_scale,
             batch_size=args.batch_size,
+            lr=args.lr,
             norm_coef=args.norm_coef,
             target_noise_coef=args.xi_coef,
             buffer_size=args.time_period,
@@ -214,10 +223,11 @@ def main(args):
         else:
             run(args, model, objective, logger, seed)
 
+    title = "SRCON" if args.env == "srcon" else f"BlackBox: {args.dataset}"
     if args.debug:
-        plotLoss(title="SRCON", path=logger.get_dir(), log=False)
+        plotLoss(title=title, path=logger.get_dir(), log=False)
     else:
-        plotReward(title="SRCON", path=logger.get_dir(), log=False)
+        plotResult(title=title, path=logger.get_dir(), log=False)
 
 
 if __name__ == "__main__":
